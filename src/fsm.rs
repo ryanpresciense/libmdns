9718f4649d6a3f54f71361a589cd8a3eec238709
Ryan Roberts <ryansroberts@gmail.com>
2020-05-27 10:39:12 +0100

    Fudge

--- src/fsm.rs
+++ src/fsm.rs
@@ -255,8 +255,10 @@ impl<AF: AddressFamily> FSM<AF> {
             trace!("sending packet to {:?}", addr);
 
             match self.socket.poll_send_to(ctx, response, addr) {
-                Poll::Ready(Err(ref ioerr)) if ioerr.kind() == std::io::ErrorKind::WouldBlock =>
-                    return Poll::Pending,
+                Poll::Ready(Err(ref ioerr)) if ioerr.kind() == std::io::ErrorKind::WouldBlock => {
+                    warn!("block sending packet {:?}", ioerr);
+                    return Poll::Pending
+                }
                 Poll::Pending => return Poll::Pending,
                 Poll::Ready(Ok(_)) => {
                     self.outgoing.pop_front();
